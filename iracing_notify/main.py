d4c908424b78542cc79460dbaa317d52490f1c82
scullionw <scuw1801@usherbrooke.ca>
2018-12-29 22:38:11 -0500

    working without persistence

---
+++ iracing_notify/main.py
@@ -0,0 +1,35 @@
+import requests
+import json
+import urllib.parse
+import sys
+from config import credentials, api_key
+
+URL_IRACING_LOGIN = 'https://members.iracing.com/membersite/Login'
+URL_API = "http://members.iracing.com/membersite/member/GetDriverStatus?friends=1&studied=1&blacklisted=1"
+NIMROD_URL = 'https://www.nimrod-messenger.io/api/v1/message'
+
+def main():
+    with requests.session() as s:
+        # Login
+        s.post(URL_IRACING_LOGIN, data=credentials)
+        # Data request
+        response = s.get(URL_API)
+
+    data = json.loads(response.text)
+
+    for driver in data['fsRacers']:
+        if 'sessionStatus' in driver:
+            if driver['sessionStatus'] != 'none':
+                name = driver['name'].replace('+', ' ')
+                name = urllib.parse.unquote(name)
+                message = f"{name} is currently driving."
+                print(message)
+                notify(message)
+
+def notify(message):
+    payload = { 'api_key' : api_key, 'message' : message }
+    requests.post(NIMROD_URL, json=payload)
+
+if __name__ == '__main__':
+    sys.exit(main())
+
