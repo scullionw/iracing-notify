5002e5b1de3af48519342f68622915816a1378bd
scullionw <scuw1801@usherbrooke.ca>
2018-12-30 23:04:14 -0500

    remember state

--- iracing_notify/main.py
+++ iracing_notify/main.py
@@ -2,7 +2,9 @@ import requests
 import json
 import urllib.parse
 import sys
+import pickle
 from config import credentials, api_key
+from pathlib import Path
 
 URL_IRACING_LOGIN = 'https://members.iracing.com/membersite/Login'
 URL_API = "http://members.iracing.com/membersite/member/GetDriverStatus?friends=1&studied=1&blacklisted=1"
@@ -11,28 +13,74 @@ NIMROD_URL = 'https://www.nimrod-messenger.io/api/v1/message'
 def main():
     print("Running..")
 
+    drivers = Drivers.load()
+
     with requests.session() as s:
         # Login
         s.post(URL_IRACING_LOGIN, data=credentials)
         # Data request
         response = s.get(URL_API)
 
+    # response.json?
     data = json.loads(response.text)
-
-    for driver in data['fsRacers']:
-        if 'sessionStatus' in driver:
-            if driver['sessionStatus'] != 'none':
-                name = driver['name'].replace('+', ' ')
-                name = urllib.parse.unquote(name)
-                message = f"{name} is currently driving."
-                print(message)
-                notify(message)
+    drivers.update(data)
+    drivers.save()
 
     print("Done.")
 
 def notify(message):
+    print(message)
     payload = { 'api_key' : api_key, 'message' : message }
     requests.post(NIMROD_URL, json=payload)
+
+def currently_driving(driver_data):
+    return 'sessionStatus' in driver_data and driver_data['sessionStatus'] != 'none'
+
+def driver_name(driver):
+    name = driver['name'].replace('+', ' ')
+    return urllib.parse.unquote(name)
+
+class Drivers:
+    save_path = Path('drivers')
+
+    def __init__(self):
+        ## Map<str, bool>
+        self.driver_map = {}
+    
+    def add(self, driver, state):
+        self.driver_map[driver] = state
+
+    def update(self, data):
+        for driver in data['fsRacers']:
+
+            name = driver_name(driver)
+            is_driving = currently_driving(driver)
+
+            if name in self.driver_map:
+                was_driving = self.driver_map[name]
+                if was_driving and is_driving:
+                    pass
+                elif was_driving and not is_driving:
+                    notify(f"{name} has stopped driving.")
+                elif not was_driving and is_driving:
+                    notify(f"{name} is now driving.")
+                else:
+                    pass
+                
+            self.driver_map[name] = is_driving
+
+
+    @classmethod
+    def load(cls):
+        if cls.save_path.exists():
+            with cls.save_path.open(mode='rb') as f:
+                return pickle.load(f)
+        else:
+            return Drivers()
+    
+    def save(self):
+        with self.save_path.open(mode='wb') as f:
+            pickle.dump(self, f)
 
 if __name__ == '__main__':
     sys.exit(main())
