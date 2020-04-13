8e8d5696ca12110e2b6df4e61fe86367b4871b28
William Scullion <williamscullion@gmail.com>
2020-04-13 16:30:39 -0400

    removed deps and asset loading

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import { Global, css } from "@emotion/core";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Drivers from "./Drivers";
 import Container from "@material-ui/core/Container";
+import "typeface-roboto";
 
 ReactDOM.render(
     <React.StrictMode>
