34838d65b7c6fb85917428f0f50dd14b794e810f
William Scullion <williamscullion@gmail.com>
2021-03-21 20:56:58 -0400

    migrate to k8s

--- api/api/database.py
+++ api/api/database.py
@@ -5,6 +5,8 @@ import os
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
+print(f"Connecting to db: {SQLALCHEMY_DATABASE_URL}")
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- scraper/scraper/__init__.py
+++ scraper/scraper/__init__.py
@@ -4,5 +4,7 @@ import logging
 import os
 
 logging.basicConfig(
-    format="%(levelname)s:     %(message)s", level=os.environ.get("LOGLEVEL", "INFO"),
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=os.environ.get("LOGLEVEL", logging.INFO),
+    datefmt='%Y-%m-%d %H:%M:%S %Z'
 )
